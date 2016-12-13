566aa2601caa6bf47504efb07e3ec1b6fae5d18c
while <while@fengyuezhu.com>
2016-12-13 17:47:52 +0800

    subtree批处理

---
+++ pull.bat
@@ -0,0 +1,4 @@
+git subtree pull --prefix=Sample/CSharp/Sample/ScorpioProto https://github.com/qingfeng346/ScorpioProto-CSharp.git src
+git subtree pull --prefix=Sample/Java/Sample/src/Scorpio https://github.com/qingfeng346/ScorpioProto-Java.git src
+git subtree pull --prefix=Sample/CPP/Sample/ScorpioProto https://github.com/qingfeng346/ScorpioProto-CPP.git src
+pause
---
+++ push.bat
@@ -0,0 +1,4 @@
+git subtree push --prefix=Sample/CSharp/Sample/ScorpioProto https://github.com/qingfeng346/ScorpioProto-CSharp.git src
+git subtree push --prefix=Sample/Java/Sample/src/Scorpio https://github.com/qingfeng346/ScorpioProto-Java.git src
+git subtree push --prefix=Sample/CPP/Sample/ScorpioProto https://github.com/qingfeng346/ScorpioProto-CPP.git src
+pause
