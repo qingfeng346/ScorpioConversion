91bf2853e747bf392aa486f898d4047cd7638554
while <qingfeng346@fengyuezhu.com>
2022-01-29 16:07:17 +0800

    update

--- Tools/build.ps1
+++ Tools/build.ps1
@@ -19,6 +19,7 @@ Write-Host "正在生成Scorpio.Conversion.Engine.nupkg..."
 dotnet pack ../ScorpioConversion/Scorpio.Conversion.Engine/Scorpio.Conversion.Engine.csproj -p:PackageVersion=$version -o ../bin/ /p:AssemblyVersion=$version | Out-Null
 
 $platforms = @("win-x86", "win-x64", "win-arm", "win-arm64", "linux-x64", "linux-musl-x64", "linux-arm", "linux-arm64", "osx-x64", "osx-arm64")
+$aipPath = ".\Install.aip"
 foreach ($platform in $platforms) {
     Write-Host "正在打包 $platform 版本..."
     $pathName = "$name-$platform"
@@ -26,6 +27,24 @@ foreach ($platform in $platforms) {
     Write-Host "正在压缩 $platform ..."
     $fileName = "$name-$version-$platform"
     Compress-Archive ../bin/$pathName/* ../bin/$fileName.zip -Force
+    if ($IsWindows -and (($platform -eq "win-x86") -or ($platform -eq "win-x64"))) {
+        Write-Host "正在生成安装包 $platform ..."
+        git checkout $aipPath
+        Get-ChildItem ..\bin\$pathName\ | ForEach-Object -Process{
+            if($_ -is [System.IO.FileInfo]) {
+                AdvancedInstaller.com /edit $aipPath /AddFile APPDIR $_.FullName
+            }
+        }
+        AdvancedInstaller.com /edit $aipPath /SetVersion $version
+        AdvancedInstaller.com /edit $aipPath /SetPackageName ..\bin\$fileName.msi -buildname DefaultBuild
+        if ($platform -eq "win-x86") {
+            AdvancedInstaller.com /edit $aipPath /SetPackageType x86 -buildname DefaultBuild
+        } elseif ($platform -eq "win-x64") {
+            AdvancedInstaller.com /edit $aipPath /SetPackageType x64 -buildname DefaultBuild
+        }
+        AdvancedInstaller.com /build $aipPath -buildslist DefaultBuild
+        git checkout $aipPath
+    }
 }
 
 Write-Host "正在生成Scorpio.Conversion.Runtime.nupkg..."
