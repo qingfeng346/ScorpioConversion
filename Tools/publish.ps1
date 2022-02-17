ed3799502b8777523733c6427237847ad42f8a0e
linyuan.yang <linyuan.yang@centurygame.com>
2022-02-17 19:18:02 +0800

    publish.ps1

--- Tools/publish.ps1
+++ Tools/publish.ps1
@@ -1,3 +1,9 @@
+<#
+pypi 上传配置 , 创建文件 ~/.pypirc，内容为下
+[pypi]
+username = __token__
+password = pypi-AgEIcHlwaS5vcmcCJDUyYTU3ODQzLTJjMzgtNGM3YS05OGYyLWI5NTFjYzM4MTZlYgACJXsicGVybWlzc2lvbnMiOiAidXNlciIsICJ2ZXJzaW9uIjogMX0AAAYgoh_vosJqfjnguPL66OQeJI9yiZqBbAeAoIfq3t_rHHo
+#>
 $files = [System.IO.Directory]::GetFiles("../bin", "*.nupkg")
 $apiKey = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("b3kyZDVxNmEyenZ1enlsZm5rcHFhaGU0eGlxMnJuZHViY2ZuMmQ2MnE3YmoyYQ=="))
 foreach ($file in $files) {
