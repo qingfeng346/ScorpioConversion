88678ee35d80336d24d22ed2810067d01477df08
linyuan.yang <linyuan.yang@centurygame.com>
2022-02-17 11:54:09 +0800

    update

--- ScorpioConversion/Scorpio.Conversion.Engine/src/Version.cs
+++ ScorpioConversion/Scorpio.Conversion.Engine/src/Version.cs
@@ -1,6 +1,6 @@
 namespace Scorpio.Conversion {
     public static class Version {
         public const string version = "1.0.0";
-        public const string date = "2022-02-02";
+        public const string date = "2022-02-17";
     }
 }
--- ScorpioProto/Python/Example/main.py
+++ ScorpioProto/Python/Example/main.py
@@ -1,10 +1,10 @@
 import sys
-sys.path.append('src')
-sys.path.append('../Scorpio.Conversion.Runtime/')
+sys.path.append('Example/src')
+sys.path.append('Scorpio.Conversion.Runtime')
 from TableManager import *
 from DefaultReader import *
 def GetReader(this, fileName):
-    return DefaultReader(open("../../" + fileName + ".data", "rb"))
+    return DefaultReader(open("../" + fileName + ".data", "rb"))
 TableManager.GetReader = GetReader
 def main():
     tttt = TableManager()
