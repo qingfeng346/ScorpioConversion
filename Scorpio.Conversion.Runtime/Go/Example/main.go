8f89cceac5d132ac2f2ee75829ed0ee1b232c596
linyuan.yang <linyuan.yang@centurygame.com>
2022-03-03 20:56:11 +0800

    update

--- Scorpio.Conversion.Runtime/Go/Example/main.go
+++ Scorpio.Conversion.Runtime/Go/Example/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/qingfeng346/ScorpioConversion/Scorpio.Conversion.Runtime/Go/ScorpioConversionRuntime"
+)
+
 func main() {
-	var reader = DefaultReader{}
+	fmt.Println("hello")
+	var reader = ScorpioConversionRuntime.DefaultReader{}
+	reader.Close()
 }
